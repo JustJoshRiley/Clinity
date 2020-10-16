f18b84a53b69c84d75305cd23aa6879cae4b8b62
Joshua Riley <Joshy@Gilly-in-Philly.localdomain>
2020-10-16 03:12:41 -0700

    finished first section, second section, started third section

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,4 @@
+//clock
 var inc = 1000;
 
 clock();
@@ -17,5 +18,22 @@ function clock() {
     document.querySelector('.minute').style.transform = `rotate(${minute}deg)`
     document.querySelector('.second').style.transform = `rotate(${second}deg)`
 }
+//accordion
+const accordion = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
 
 setInterval(clock, inc);
